fdf2b642397247f4c4ec7d1c8a0ca539c261c6cf
Spencer Stream <sprewstream@gmail.com>
2023-12-09 17:05:47 -0600

    Promoting the game to the site root and fixing issues with link paths to fix broken build in production

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,246 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+<script lang="ts">
+	import { invalidateAll } from '$app/navigation';
+	import { base } from '$app/paths';
+	import { confetti } from '@neoconfetti/svelte';
+	import type { PageData } from './$types';
+	import Controls from './Controls.svelte';
+	import { Game } from './game';
+	import { reduced_motion } from './reduced-motion';
+
+	interface IProps {
+		data: PageData;
+	}
+
+	let { data } = $props<IProps>();
+
+	let badGuess = $state(false);
+
+	/** Whether or not the user has won */
+	let won = $derived(data.answers.at(-1) === 'xxxxx');
+
+	/** The index of the current guess */
+	let i = $derived(won ? -1 : data.answers.length);
+
+	/** The current guess */
+	let currentGuess = $derived(data.guesses[i] || '');
+
+	/** Whether the current guess can be submitted */
+	let submittable = $derived(currentGuess.length === 5);
+
+	function update(key: string) {
+		if (key === 'backspace') {
+			data.guesses[i] = data.guesses[i].slice(0, 1);
+			if (badGuess) badGuess = false;
+		} else if (currentGuess.length < 5) {
+			data.guesses[i] += key;
+		}
+	}
+
+	function submit() {
+		const game = new Game(localStorage.getItem('sverdle') ?? '');
+
+		if (!game.enter([...currentGuess])) {
+			badGuess = true;
+		}
+
+		localStorage.setItem('sverdle', game.toString());
+		invalidateAll();
+	}
+
+	function restart() {
+		localStorage.removeItem('sverdle');
+		invalidateAll();
+	}
+
+	function handleKey(event: any) {
+		const key = event.detail.key;
+
+		switch (key) {
+			case 'enter':
+				submit();
+				break;
+			case 'restart':
+				restart();
+				break;
+			default:
+				update(key);
+				break;
+		}
+	}
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Sverdle</title>
+	<meta name="description" content="A Wordle clone written in SvelteKit" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+<h1 class="visually-hidden">Sverdle</h1>
 
-		to your new<br />SvelteKit app
-	</h1>
+<div class="form">
+	<a class="how-to-play" href={`${base}/how-to-play`}>How to play</a>
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
+	<div class="grid" class:playing={!won} class:bad-guess={badGuess}>
+		{#each Array.from(Array(6).keys()) as row (row)}
+			{@const current = row === i}
+			<h2 class="visually-hidden">Row {row + 1}</h2>
+			<div class="row" class:current>
+				{#each Array.from(Array(5).keys()) as column (column)}
+					{@const guess = current ? currentGuess : data.guesses[row]}
+					{@const answer = data.answers[row]?.[column]}
+					{@const value = guess?.[column] ?? ''}
+					{@const selected = current && column === guess.length}
+					{@const exact = answer === 'x'}
+					{@const close = answer === 'c'}
+					{@const missing = answer === '_'}
+					<div class="letter" class:exact class:close class:missing class:selected>
+						{value}
+						<span class="visually-hidden">
+							{#if exact}
+								(correct)
+							{:else if close}
+								(present)
+							{:else if missing}
+								(absent)
+							{:else}
+								empty
+							{/if}
+						</span>
+						<input name="guess" disabled={!current} type="hidden" {value} />
+					</div>
+				{/each}
+			</div>
+		{/each}
+	</div>
+	<Controls on:key={handleKey} {data} {won} {submittable} />
+</div>
 
-	<Counter />
-</section>
+{#if won}
+	<div
+		style="position: absolute; left: 50%; top: 30%"
+		use:confetti={{
+			particleCount: $reduced_motion ? 0 : undefined,
+			force: 0.7,
+			stageWidth: window.innerWidth,
+			stageHeight: window.innerHeight,
+			colors: ['#ff3e00', '#40b3ff', '#676778']
+		}}
+	/>
+{/if}
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
+	.form {
 		width: 100%;
 		height: 100%;
-		top: 0;
-		display: block;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		gap: 1rem;
+		flex: 1;
+	}
+
+	.how-to-play {
+		color: var(--color-text);
+	}
+
+	.how-to-play::before {
+		content: 'i';
+		display: inline-block;
+		font-size: 0.8em;
+		font-weight: 900;
+		width: 1em;
+		height: 1em;
+		padding: 0.2em;
+		line-height: 1;
+		border: 1.5px solid var(--color-text);
+		border-radius: 50%;
+		text-align: center;
+		margin: 0 0.5em 0 0;
+		position: relative;
+		top: -0.05em;
+	}
+
+	.grid {
+		--width: min(100vw, 40vh, 380px);
+		max-width: var(--width);
+		align-self: center;
+		justify-self: center;
+		width: 100%;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-start;
+	}
+
+	.grid .row {
+		display: grid;
+		grid-template-columns: repeat(5, 1fr);
+		grid-gap: 0.2rem;
+		margin: 0 0 0.2rem 0;
+	}
+
+	@media (prefers-reduced-motion: no-preference) {
+		.grid.bad-guess .row.current {
+			animation: wiggle 0.5s;
+		}
+	}
+
+	.grid.playing .row.current {
+		filter: drop-shadow(3px 3px 10px var(--color-bg-0));
+	}
+
+	.letter {
+		aspect-ratio: 1;
+		width: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+		box-sizing: border-box;
+		text-transform: lowercase;
+		border: none;
+		font-size: calc(0.08 * var(--width));
+		border-radius: 2px;
+		background: white;
+		margin: 0;
+		color: rgba(0, 0, 0, 0.7);
+	}
+
+	.letter.missing {
+		background: rgba(255, 255, 255, 0.5);
+		color: rgba(0, 0, 0, 0.5);
+	}
+
+	.letter.exact {
+		background: var(--color-theme-2);
+		color: white;
+	}
+
+	.letter.close {
+		border: 2px solid var(--color-theme-2);
+	}
+
+	@keyframes wiggle {
+		0% {
+			transform: translateX(0);
+		}
+		10% {
+			transform: translateX(-2px);
+		}
+		30% {
+			transform: translateX(4px);
+		}
+		50% {
+			transform: translateX(-6px);
+		}
+		70% {
+			transform: translateX(+4px);
+		}
+		90% {
+			transform: translateX(-2px);
+		}
+		100% {
+			transform: translateX(0);
+		}
 	}
 </style>
--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,7 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: '404.html',
+			fallback: undefined,
 			precompress: false,
 			strict: true
 		}),
