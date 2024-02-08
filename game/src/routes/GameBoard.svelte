7bdbe3bf855b09a66148fad0be00e514d3088965
Spencer Stream <sprewstream@gmail.com>
2024-02-07 20:25:19 -0600

    Adding typo underline for invalid words

--- game/src/routes/GameBoard.svelte
+++ game/src/routes/GameBoard.svelte
@@ -94,8 +94,30 @@
 		color: var(--color-text-invalid);
 	}
 
+	.game.playing.invalid .row.current .letter::after,
+	.game.playing.invalid .row.current .letter::before {
+		height: inherit;
+		width: 50%;
+		background-size: 4px 130%;
+		content: '';
+		position: absolute;
+	}
+
+	.game.playing.invalid .row.current .letter::before {
+		top: -2px;
+		background-image: linear-gradient(45deg, var(--color-text-invalid) 35%, transparent 0),
+			linear-gradient(-45deg, var(--color-text-invalid) 35%, transparent 0);
+	}
+
+	.game.playing.invalid .row.current .letter::after {
+		top: 0px;
+		background-image: linear-gradient(45deg, white 35%, transparent 0),
+			linear-gradient(-45deg, white 35%, transparent 0);
+	}
+
 	.letter {
 		height: 4vh;
+		position: relative;
 		display: flex;
 		align-items: center;
 		justify-content: center;
