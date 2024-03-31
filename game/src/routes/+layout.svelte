ddf28ec0281cab629d9a02155c3285c1b26b8802
Spencer Stream <sprewstream@gmail.com>
2024-03-31 12:31:25 -0500

    Moving CSS colors to their own file

--- game/src/routes/+layout.svelte
+++ game/src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+	import './colors.css';
+	import './styles.css';
+
 	import { browser } from '$app/environment';
 	import { createThemeState } from '$lib/api';
 	import type { Theme } from '$lib/types';
 	import { setContext } from 'svelte';
 	import Menu from './Menu.svelte';
-	import './styles.css';
 
 	const initialTheme = browser
 		? (localStorage.getItem('theme') as Theme) ?? 'system-theme'
