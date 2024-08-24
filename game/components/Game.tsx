38d9216bf2741ccc47825f8f7a9ece6fee0511cf
Spencer Stream <sprewstream@gmail.com>
2024-08-24 11:25:31 -0500

    Setting up layout of the home and game screens

--- game/components/Game.tsx
+++ game/components/Game.tsx
@@ -1,5 +1,7 @@
-import { View } from "react-native";
+import { View, StyleSheet} from "react-native";
 import { ThemedText } from "./ThemedText";
+import { Keyboard } from "./Keyboard";
+import { GameBoard } from "./GameBoard";
 
 interface IProps {
     numberOfGames: number;
@@ -8,7 +10,24 @@ interface IProps {
 export function Game(props: IProps) {
     const {numberOfGames} = props;
 
-    return <View>
-        <ThemedText>Game {numberOfGames}</ThemedText>
+    return <View style={styles.gameWrapper}>
+        <View style={styles.gamesArea}>
+            {[...Array(numberOfGames)].map((_, gameIndex) => <GameBoard key={gameIndex} gameIndex={gameIndex}/> )}
+        </View>
+        <Keyboard />
     </View>
 }
+
+const styles = StyleSheet.create({
+    gameWrapper: {
+      flex: 1,
+      backgroundColor: '#0022f8',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    gamesArea: {
+        flex: 1,
+        backgroundColor: '#00ff00',
+        flexGrow: 1,
+    }
+  });
