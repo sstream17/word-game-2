1e41277bb55fbdf79b594641a5721cf471ded4fc
Spencer Stream <sprewstream@gmail.com>
2024-08-23 20:07:36 -0500

    Center align header titles

--- game/app/_layout.tsx
+++ game/app/_layout.tsx
@@ -4,7 +4,7 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <ThemeProvider value={ DarkTheme }>
-      <Stack>
+      <Stack screenOptions={{headerTitleAlign: "center"}}>
         <Stack.Screen name="index" options={{title: "Word Game"}}/>
         <Stack.Screen name="1" options={{title: ""}} />
         <Stack.Screen name="2" options={{title: ""}} />
