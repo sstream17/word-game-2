7cc56d1bd7d736b2f28cde1d4ec5fa1f260a6a08
Spencer Stream <sprewstream@gmail.com>
2024-08-24 13:08:30 -0500

    Generate static routes for each game

--- game/app/[num].tsx
+++ game/app/[num].tsx
@@ -3,6 +3,12 @@ import { Redirect, useLocalSearchParams } from "expo-router";
 
 const validGames = [1, 2, 4];
 
+export async function generateStaticParams(): Promise<Record<string, string>[]> {
+  // Return an array of params to generate static HTML files for.
+  // Each entry in the array is a page for a game.
+  return validGames.map(gameNumber => ({ num: `${gameNumber}` }));
+}
+
 export default function GamePage() {
   const {num} = useLocalSearchParams<{num: string}>();
 
