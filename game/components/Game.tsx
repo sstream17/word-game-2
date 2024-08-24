6fd7be1f782a5e82544a612d3d1ca70f0fb0828c
Spencer Stream <sprewstream@gmail.com>
2024-08-24 17:48:30 -0500

    Starting keyboard functionality

--- game/components/Game.tsx
+++ game/components/Game.tsx
@@ -1,4 +1,5 @@
-import { StyleSheet, View } from "react-native";
+import { useCallback, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import { GameBoard } from "./GameBoard";
 import { Keyboard } from "./Keyboard";
 
@@ -9,14 +10,35 @@ interface IProps {
 export function Game(props: IProps) {
   const { numberOfGames } = props;
 
+  const [guess, setGuess] = useState("");
+
+  const updateGuess = useCallback(
+    (newKey: string) => {
+      if (newKey === "backspace") {
+        setGuess((prev) => prev.slice(0, -1));
+        return;
+      }
+
+      if (guess.length === 5) {
+        return;
+      }
+
+      setGuess((prev) => `${prev}${newKey}`);
+    },
+    [guess.length],
+  );
+
+  const submitGuess = useCallback(() => {}, []);
+
   return (
     <View style={styles.gameWrapper}>
+      <Text>{guess}</Text>
       <View style={styles.gamesArea}>
         {[...Array(numberOfGames)].map((_, gameIndex) => (
           <GameBoard key={gameIndex} gameIndex={gameIndex} />
         ))}
       </View>
-      <Keyboard />
+      <Keyboard updateGuess={updateGuess} submitGuess={submitGuess} />
     </View>
   );
 }
--- game/components/Keyboard.tsx
+++ game/components/Keyboard.tsx
@@ -1,10 +1,41 @@
-import { View, StyleSheet } from "react-native";
-import { ThemedText } from "./ThemedText";
+import { StyleSheet, View } from "react-native";
+import { KeyboardKey } from "./KeyboardKey";
 
-export function Keyboard() {
+interface IProps {
+  updateGuess: (letter: string) => void;
+  submitGuess: VoidFunction;
+}
+
+export function Keyboard(props: IProps) {
+  const { updateGuess, submitGuess } = props;
+
   return (
     <View style={styles.container}>
-      <ThemedText>Keyboard</ThemedText>
+      <View style={styles.keyboardRow}>
+        {[..."qwertyuiop"].map((char) => (
+          <KeyboardKey key={char} letter={char} onClick={updateGuess} />
+        ))}
+      </View>
+      <View style={styles.keyboardRow}>
+        {[..."asdfghjkl"].map((char) => (
+          <KeyboardKey key={char} letter={char} onClick={updateGuess} />
+        ))}
+      </View>
+      <View style={styles.keyboardRow}>
+        <KeyboardKey
+          letter={"backspace"}
+          onClick={updateGuess}
+          icon="backspace-outline"
+        />
+        {[..."zxcvbnm"].map((char) => (
+          <KeyboardKey key={char} letter={char} onClick={updateGuess} />
+        ))}
+        <KeyboardKey
+          letter={"enter"}
+          onClick={submitGuess}
+          icon="send-outline"
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +44,15 @@ const styles = StyleSheet.create({
   container: {
     height: 300,
     display: "flex",
+    flexDirection: "column",
     backgroundColor: "#0000ff",
     alignItems: "center",
     justifyContent: "center",
+    gap: 8,
+  },
+  keyboardRow: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 4,
   },
 });
