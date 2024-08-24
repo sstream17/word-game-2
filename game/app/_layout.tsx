f3743b285e20f15f913f6463533aaa0e988bfad2
Spencer Stream <sprewstream@gmail.com>
2024-08-24 12:47:20 -0500

    Adding menu

--- game/app/_layout.tsx
+++ game/app/_layout.tsx
@@ -1,10 +1,11 @@
+import Menu from "@/components/Menu";
 import { DarkTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
     <ThemeProvider value={ DarkTheme }>
-      <Stack screenOptions={{headerTitleAlign: "center"}}>
+      <Stack screenOptions={{headerTitleAlign: "center", headerRight: () => <Menu /> }}>
         <Stack.Screen name="index" options={{title: "Word Game"}}/>
         <Stack.Screen name="[num]" options={{title: ""}} />
         <Stack.Screen name="how-to-play" options={{title: "How to play"}} />
---
+++ game/components/Menu.tsx
@@ -0,0 +1,55 @@
+import { Link } from "expo-router";
+import { useCallback, useState } from "react";
+import { Button, Modal, Pressable, StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
+
+export default function Menu() {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const onOpen = useCallback(() => {
+        setIsVisible(true);
+    }, []);
+
+    const onClose = useCallback(() => {
+        setIsVisible(false);
+    }, []);
+      
+    return (
+        <>
+        <Button title="menu" onPress={onOpen}/>
+        <Modal transparent visible={isVisible}>
+      <TouchableWithoutFeedback onPress={onClose}>
+        <View style={styles.fullScreen} />
+      </TouchableWithoutFeedback>
+      <View style={styles.modalContent}>
+      <Link href="/how-to-play" asChild>
+      <Pressable>
+        
+      <Text>How to play</Text>
+      </Pressable>
+      </Link>
+      <Link href="/stats" asChild>
+      <Pressable>
+        
+      <Text>Stats</Text>
+      </Pressable>
+      </Link>
+      </View>
+    </Modal>
+    </>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    fullScreen: {
+        height: '100%',
+        width: '100%',
+    },
+    modalContent: {
+        height: '25%',
+        backgroundColor: '#fff',
+        borderRadius: 8,
+        position: 'absolute',
+        top: 16,
+        right: 16,
+      },
+  });
