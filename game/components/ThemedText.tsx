81b321f1ddc21df1f1d2d0aa78d1bde9062e1c7b
Spencer Stream <sprewstream@gmail.com>
2025-03-20 17:27:59 -0500

    fixup themed text fontFamily

--- game/components/ThemedText.tsx
+++ game/components/ThemedText.tsx
@@ -1,5 +1,5 @@
 import { memo } from "react";
-import { Platform, StyleSheet, Text, type TextProps } from "react-native";
+import { StyleSheet, Text, type TextProps } from "react-native";
 
 export const ThemedText = memo(function ThemedText(props: TextProps) {
   const { style, ...rest } = props;
@@ -10,9 +10,6 @@ export const ThemedText = memo(function ThemedText(props: TextProps) {
 
 const styles = StyleSheet.create({
   text: {
-    fontFamily: Platform.select({
-      android: "Nunito_400Regular",
-      ios: "Nunito-Regular",
-    }),
+    fontFamily: "Nunito_400Regular",
   },
 });
