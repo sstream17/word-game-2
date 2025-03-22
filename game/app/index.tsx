88504ff4bb7dcc1259421f1de9e93f1a4bd256ed
Spencer Stream <sprewstream@gmail.com>
2025-03-22 17:07:44 -0500

    Add version info to the home page

--- game/app/index.tsx
+++ game/app/index.tsx
@@ -2,27 +2,31 @@ import { Pressable, StyleSheet } from "react-native";
 
 import GradientBackgroundView from "@/components/GradientBackgroundView";
 import { ThemedText } from "@/components/ThemedText";
+import { VersionInfo } from "@/components/VersionInfo";
 import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <GradientBackgroundView style={styles.container}>
-      <Link href="/1" asChild>
-        <Pressable>
-          <ThemedText>Classic</ThemedText>
-        </Pressable>
-      </Link>
-      <Link href="/2" asChild>
-        <Pressable>
-          <ThemedText>Duo</ThemedText>
-        </Pressable>
-      </Link>
-      <Link href="/4" asChild>
-        <Pressable>
-          <ThemedText>Quad</ThemedText>
-        </Pressable>
-      </Link>
-    </GradientBackgroundView>
+    <>
+      <GradientBackgroundView style={styles.container}>
+        <Link href="/1" asChild>
+          <Pressable>
+            <ThemedText>Classic</ThemedText>
+          </Pressable>
+        </Link>
+        <Link href="/2" asChild>
+          <Pressable>
+            <ThemedText>Duo</ThemedText>
+          </Pressable>
+        </Link>
+        <Link href="/4" asChild>
+          <Pressable>
+            <ThemedText>Quad</ThemedText>
+          </Pressable>
+        </Link>
+      </GradientBackgroundView>
+      <VersionInfo />
+    </>
   );
 }
 
