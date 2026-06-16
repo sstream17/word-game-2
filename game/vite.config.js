1883908a8188a7550821a234632bbb42501fbf7d
Spencer Stream <sprewstream@gmail.com>
2026-06-16 18:16:40 -0500

    Add manifest.json and include PWA plugin in vite config

--- game/vite.config.js
+++ game/vite.config.js
@@ -1,9 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA(),
+	],
 	define: {
-	    APP_VERSION: JSON.stringify(process.env.npm_package_version),
+		APP_VERSION: JSON.stringify(process.env.npm_package_version),
 	},
 });
