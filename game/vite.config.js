dee6504a89b73a8fa9f0987521f1729b5f4bb1b4
Spencer Stream <sprewstream@gmail.com>
2026-06-16 20:27:39 -0500

    Add SvelteKitPWA options

--- game/vite.config.js
+++ game/vite.config.js
@@ -5,7 +5,13 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-		SvelteKitPWA(),
+		SvelteKitPWA({
+			registerType: 'autoUpdate',
+			manifestFilename: 'manifest.json',
+			devOptions: {
+				enabled: true,
+			},
+		}),
 	],
 	define: {
 		APP_VERSION: JSON.stringify(process.env.npm_package_version),
